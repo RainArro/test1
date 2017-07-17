fff1c1063d07eca4223982b43678887bf315ddcc
Rain Arro <rainarro96@gmail.com>
2017-07-17 17:09:43 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var scrolled = 0;
+$(document).ready(function() {
+
+  $(".navbar-toggle").click(function() {
+    $(this).toggleClass('open');
+
+  });
+
+  $(".form-control").focus(function() {
+    $(this).parent().addClass("focus")
+  });
+  $(".form-control").blur(function() {
+    $(this).parent().removeClass("focus")
+  });
+
+
+});
+$('.sidebar-inner').css("height", ($('.article').height() - 135));
+$(window).resize(function() {
+  $('.sidebar-inner').css("height", ($('.article').height() - 135));
+});
